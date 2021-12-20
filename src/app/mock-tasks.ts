aefa567ecbe934a0468372d96c073e7b4373372c
Yuliana Vargas <yulii.vargas@gmail.com>
2021-12-20 16:03:43 -0300

    Lista de items

--- src/app/mock-tasks.ts
+++ src/app/mock-tasks.ts
@@ -1,13 +1,13 @@
-import { Task } from "./Task"
+import {Task} from "./Task"
 
-export const TASK: Task[] = [
+export const TASKS: Task[] = [
     {
         id: 1,
         text: "Terminar primer modulo de angular",
         day: 'Agosto 5 a las 12:00',
         reminder: true
-
     },
+
     {
         id: 2,
         text: "Hacer las compras para la casa",
@@ -21,7 +21,8 @@ export const TASK: Task[] = [
         day: 'Agosto 5 a las 19:00',
         reminder: false
 
-    }, {
+    }, 
+    {
         id: 4,
         text: "Leer mi libro favorito",
         day: 'Agosto 5 a las 21:00',
